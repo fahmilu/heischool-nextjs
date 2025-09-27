48f32c97189f1a894110b9799fa616cf5da75472
Fahmilu Kurniawan <363205+fahmilu@users.noreply.github.com>
2025-09-27 18:06:24 +0700

    update form

---
+++ src/app/form/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen container max-w-[800px] mx-auto'>
+        <iframe src="https://forms.littlelives.com/interest-form/123607-hei-schools-senayan?lang=eng&embed=true" width="100%" height="100%" frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
+    </div>
+  )
+}
+
+export default page
