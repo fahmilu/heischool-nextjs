993515b2fe56376009fe1585d4e02b9511bab596
Carlito Morales <carlito_morales@yahoo.co.id>
2025-12-08 11:49:16 +0700

    Add Noyh font family and DM Sans import to global styles; clean up layout.js by removing local font definitions

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,3 @@
-import localFont from "next/font/local";
-
-import { DM_Sans } from "next/font/google";
 import "@/public/styles/globals.scss";
 // import Header from "@/components/Partials/Header";
 import Footer from "@/components/Partials/Footer";
@@ -8,53 +5,6 @@ import { ReactLenis } from "lenis/react";
 import DynamicHeaderWrapper from "@/components/Partials/DynamicHeaderWrapper";
 import FloatingButton from "@/components/floatingButton";
 import StoreProvider from "@/redux/StoreProvider";
-
-const DMSans = DM_Sans({
-  variable: "--font-dmsans",
-  weight: ["400", "600", "700"],
-  subsets: ["latin"],
-});
-
-const Noyh = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Noyh-Black.woff",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-Heavy.woff",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-Bold.woff",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-Medium.woff",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-SemiLight.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Noyh-Light.woff",
-      weight: "200",
-      style: "normal",
-    }
-  ],
-  variable: "--font-noyh",
-});
 
 export const metadata = {
   title: "Hei Schools Indonesia",
@@ -64,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${DMSans.variable} ${Noyh.variable} antialiased`} >
+      <body className="antialiased" >
         <StoreProvider>
           <DynamicHeaderWrapper />
           <ReactLenis root>
